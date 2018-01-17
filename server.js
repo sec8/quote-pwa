e03573653bb91ff97fede421e8fa1c6aa14122f2
sec8 <lavalampeh@gmail.com>
2018-01-17 21:53:45 +0100

    Add request package for express & fetch quote data from forismatic on the server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import request from 'request';
 
 const app = express();
+const URL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&json=?";
 
 app.get('/', function (req, res) {
   res.send('Hello World!!');
+});
+
+request.get(URL, (error, response, data) => {
+  let json = JSON.parse(data);
+  console.log(json);
 });
 
 app.listen(3001, function () {
