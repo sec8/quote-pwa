4c6afb815b7419931995deb20af9de481c1a06b7
sec8 <lavalampeh@gmail.com>
2018-01-17 12:09:40 +0100

    Extend navigation with a sidebar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
-import ToolBar from 'material-ui/Toolbar'
+import ToolBar from 'material-ui/Toolbar';
+import Drawer from 'material-ui/Drawer';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import { MenuItem } from 'material-ui/Menu';
 
 const styles = {
   root: {
@@ -17,17 +19,34 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  sideBar: {
+    width: 250
+  }
 };
 
 class Navigation extends Component {
+
+  state = {
+    open: false,
+  };
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render() {
     const { classes, title } = this.props;
     
+    const sideBarMenu = (
+      <div className={classes.sideBar}>
+        <MenuItem onClick={this.handleToggle}>Home</MenuItem>
+        <MenuItem onClick={this.handleToggle}>About</MenuItem>
+      </div>
+    );
+
     return (
       <div className={classes.root}>
         <AppBar position="fixed">
           <ToolBar>
-            <IconButton className={classes.iconButton} color="contrast" aria-label="Menu">
+            <IconButton className={classes.iconButton} onClick={this.handleToggle} color="contrast" aria-label="Menu">
               <MenuIcon />
             </IconButton>
             <Typography type="title" color="inherit" className={classes.flex}>
@@ -35,8 +54,11 @@ class Navigation extends Component {
             </Typography>
           </ToolBar>
         </AppBar>
+        <Drawer open={this.state.open} onClose={this.handleToggle}>
+          {sideBarMenu}
+        </Drawer>
       </div>
-    )
+    );
   }
 }
 
