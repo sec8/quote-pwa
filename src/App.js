c5a8cb77a17980353e85418650f98c8d8ed67ad7
sec8 <lavalampeh@gmail.com>
2018-01-17 11:17:38 +0100

    Add some basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Page = ({title}) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">{title}</h1>
+    </header>
+    <p className="App-intro">
+       To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <p><Link to="/">Home</Link></p>
+    <p><Link to="/about">About</Link></p>
+  </div>
+);
+
+const Home = (props) => (
+  <Page title="Home" />
+);
+
+const About = (props) => (
+  <Page title="About" />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
     );
   }
 }
