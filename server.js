f8eaa21a652dd32ec450498d5fbbd9e6937d9a00
sec8 <lavalampeh@gmail.com>
2018-01-19 02:21:46 +0100

    Setup server for serving assets in prodcution

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import request from 'request';
+import path from 'path';
 
 const app = express();
 const URL = "https://talaikis.com/api/quotes/random/";
@@ -7,6 +8,7 @@ const URL = "https://talaikis.com/api/quotes/random/";
 let quote;
 
 app.enable("trust proxy");
+app.use(express.static('./build'));
 
 const getQuote = () => {
   request.get(URL, (error, response, data) => {
@@ -20,7 +22,9 @@ app.get('/quote', function (req, res) {
   res.send(quote);
 });
 
-//app.use(express.static("./build"));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
