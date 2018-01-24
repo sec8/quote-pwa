52ecfb4ea21041e06ddf296fb3ea7f0903598c52
sec8 <lavalampeh@gmail.com>
2018-01-24 22:09:27 +0100

    Improve readability of the quotelist

--- src/QuoteListing.js
+++ src/QuoteListing.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import Divider from 'material-ui/Divider';
 
+const styles = {
+  quoteText: {
+    marginTop: "1.35em",
+  },
+  quoteAuthor: {
+    marginBottom: "1.35em",
+  },
+};
+
 const QuoteListing = (props) => {
-  const { data } = props;
+  const { data, classes } = props;
 
   return (
     <div>
-      <Typography type="body2" gutterBottom>
+      <Typography className={classes.quoteText} type="body2" gutterBottom>
         {data.quoteText}
       </Typography>
-      <Typography type="body1">
+      <Typography className={classes.quoteAuthor} type="caption" gutterBottom>
         - {data.quoteAuthor}
       </Typography>
       <Divider />
@@ -18,6 +28,6 @@ const QuoteListing = (props) => {
   )
 }
 
-export default QuoteListing;
+export default withStyles(styles)(QuoteListing);
 
 
