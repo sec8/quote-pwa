8252c3ae4b11bf219b28696fb231c147c477a50b
sec8 <lavalampeh@gmail.com>
2018-02-09 01:19:05 +0100

    Add roboto font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import 'typeface-roboto';
 
 import Navigation from './Navigation';
 import RandomQuote from './RandomQuote';
