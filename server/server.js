95cc6f0873cb8e79c8f6f69d2ec3985bb408238f
sec8 <lavalampeh@gmail.com>
2018-02-12 22:47:49 +0100

    Add SSR (just app shell) for entry page

---
+++ server/server.js
@@ -0,0 +1,91 @@
+import express from 'express';
+import request from 'request';
+import path from 'path';
+import compression from 'compression';
+import { renderToString } from 'react-dom/server';
+import React, { Component } from 'react';
+import { StaticRouter } from 'react-router-dom';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { SheetsRegistry } from 'react-jss/lib/jss';
+import { MuiThemeProvider, createGenerateClassName } from 'material-ui/styles';
+import fs from 'fs';
+import App from '../babel-src/App';
+import theme from '../babel-src/theme';
+
+const app = express();
+const URL = "https://talaikis.com/api/quotes/random/";
+const CACHE_URL = "https://talaikis.com/api/quotes/";
+
+let randomQuote;
+let quotes;
+
+app.use(compression());
+app.enable("trust proxy");
+
+const serverRender = (req, res) => {
+  const htmlFile = path.resolve(__dirname, '..', 'build', 'index.html');
+  const sheetsRegistry = new SheetsRegistry();
+  const generateClassName = createGenerateClassName();
+
+  fs.readFile(htmlFile, 'utf8', (err, htmlData) => {
+    if(err) {
+      console.error('err', err);
+      return res.status(404).end()
+    }
+    const context = {};
+    const html = renderToString(
+      <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+        <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+          <StaticRouter location={req.url} context={ context }>
+            <App />
+          </StaticRouter>
+        </MuiThemeProvider>
+      </JssProvider>
+    )
+
+    const css = sheetsRegistry.toString();
+
+    return res.send(
+      htmlData.replace(
+        '<div id="root"></div>',
+        `<div id="root">${html}</div>
+         <style id="jss-server-side">${css}</style>`
+      )
+    )
+  })
+}
+
+const getRandomQuote = () => {
+  request.get(URL, (error, response, data) => {
+    console.log(data);
+    randomQuote = JSON.parse(data);
+  });
+};
+
+const getQuotes = () => {
+  request.get(CACHE_URL, (error, response, data) => {
+    quotes = JSON.parse(data);
+  });
+};
+
+//seems like a really ugly fix to the first request failed problem
+getRandomQuote();
+getQuotes();
+
+app.get('/quote', function (req, res) {
+  getRandomQuote();
+  res.send(randomQuote);
+});
+
+app.get('/quotes', function (req, res) {
+  getQuotes();
+  res.send(quotes);
+});
+
+app.get('/', serverRender);
+app.use(express.static('./build'));
+app.use('/*', serverRender);
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
