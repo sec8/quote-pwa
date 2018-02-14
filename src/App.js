fe91c6e4c9fac060b36f3642eefbb80307c0fde1
sec8 <lavalampeh@gmail.com>
2018-02-14 13:27:11 +0100

    Include roboto font over cdn instead of import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import 'typeface-roboto';
 import RandomQuote from './RandomQuote';
 import QuoteList from './QuoteList';
 import Page from './Page';
